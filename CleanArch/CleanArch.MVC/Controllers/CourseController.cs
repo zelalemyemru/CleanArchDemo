2dfbe03e1b2a6afe424e4ada57fbab47bc4eaa77
Zelalemyemru <zelalemyemru@outlook.com>
2020-09-11 03:12:33 -0400

    final commit of presentation layar

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+	[Authorize]
+    public class CourseController : Controller
+    {
+		private ICourseService _courseService;
+
+		public CourseController(ICourseService courseService)
+		{
+			_courseService = courseService;
+		}
+
+		// get
+		public IActionResult Index()
+		{
+			CourseViewModel model = _courseService.GetCourses();
+			return View(model);
+		}
+
+	}
+}
