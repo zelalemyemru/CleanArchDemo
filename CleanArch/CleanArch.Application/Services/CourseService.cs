048d9caf7b3d8b2d1b2c5d13acdf3357d448cc5d
Zelalemyemru <zelalemyemru@outlook.com>
2020-09-11 01:55:59 -0400

    Commit for implementing Repository

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+	public class CourseService : ICourseService
+	{
+		// call repositoty or dependecy injection 
+
+		private ICourseRepository _courseRepository;
+
+		//Constractor and inject repository
+		public CourseService(ICourseRepository courseRepository)
+		{
+			_courseRepository = courseRepository;
+		}
+		public CourseViewModel GetCourses()
+		{
+			return new CourseViewModel()
+			{
+				Courses = _courseRepository.GetCourses()
+			};
+		}
+	}
+}
