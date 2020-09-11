13f37b3ebdaab7868ef69bda0ce2b2d7a96159a5
Zelalemyemru <zelalemyemru@outlook.com>
2020-09-11 01:20:51 -0400

    Commit service and Repository Interface, CourseViewModel

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.ViewModels
+{
+	public class CourseViewModel
+	{
+		// list all courses in course
+		public IEnumerable<Course> Courses { get; set; }
+
+	}
+}
---
+++ CleanArch/CleanArch.Domain/Interface/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interface
+{
+	public interface ICourseRepository
+	{
+		//
+		IEnumerable<Course> GetCourses();
+	}
+}
